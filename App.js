2bbf01a28a3d516a72411ae5450e0269b3a8ac50
Cameron Graybill <camerongraybill@gmail.com>
2016-09-15 22:45:03 -0400

    Working on user pages, fixed some reporting stuff

--- App.js
+++ App.js
@@ -32,21 +32,25 @@ var auth = require('passport-local-authenticate');
 	//set some app settings
 	app.use(express.static(__dirname + '/hostedItems'));
 	app.set('view engine', 'jade');
+	app.use(require('morgan')('combined'));
+	app.use(require('cookie-parser')());
+	app.use(require('body-parser').urlencoded({ extended: true }));
+	app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 	//Utility stuff
 	function buildScoreboard(callback){
-		players.find({meleeRating: {$exists: true}}, {meleeRating:1, name:1, meleeWins:1, meleeLosses:1, meleeMain:1}).sort({meleeRating: -1}, function(err, meleePlayers){
+		players.find({meleeRating: {$exists: true}}, {meleeRating:1, meleeWins:1, meleeLosses:1, meleeMain:1, displayName:1, username:1}).sort({meleeRating: -1}, function(err, meleePlayers){
 			callback({game: 'melee', data: meleePlayers});
 		});
-		players.find({pmRating: {$exists: true}}, {pmRating:1, name:1, pmWins:1, pmLosses:1, pmMain:1}).sort({pmRating: -1}, function(err, pmPlayers){
+		players.find({pmRating: {$exists: true}}, {pmRating:1, pmWins:1, pmLosses:1, pmMain:1, displayName:1, username:1}).sort({pmRating: -1}, function(err, pmPlayers){
 			callback({game: 'pm', data: pmPlayers});
 		});
-		players.find({smashFourRating: {$exists: true}}, {smashFourRating:1, name:1, smashFourWins:1, smashFourLosses:1, smashFourMain:1}).sort({smashFourRating: -1}, function(err, smashFourPlayers){
+		players.find({smashFourRating: {$exists: true}}, {smashFourRating:1, smashFourWins:1, smashFourLosses:1, smashFourMain:1, displayName:1, username:1}).sort({smashFourRating: -1}, function(err, smashFourPlayers){
 			callback({game: 'smashFour', data: smashFourPlayers});
 		});
 	}
-	
-	passport.use(new Strategy(function(username, password, callback){
-		players.find({name: username}, function(err, docs){
+	//Passport authentication and login
+	passport.use(new Strategy(function(uname, password, callback){
+		players.find({username: uname}, function(err, docs){
 			var user = docs[0];
 			if(err){return callback(err);}
 			if(!user){return callback(null, false);}
@@ -58,25 +62,22 @@ var auth = require('passport-local-authenticate');
 	}));
 	
 	passport.serializeUser(function(user, callback){
-		callback(null, user['name']);
+		callback(null, user['username']);
 	});
 	
-	passport.deserializeUser(function(id, callback){
-		players.find({name:id}, function(err, docs){
+	passport.deserializeUser(function(uname, callback){
+		players.find({username:uname}, function(err, docs){
 			if(err) {return callback(err)};
 			callback(null, docs[0]);
 		});
 	});
-	app.use(require('morgan')('combined'));
-	app.use(require('cookie-parser')());
-	app.use(require('body-parser').urlencoded({ extended: true }));
-	app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 	app.use(passport.initialize());
 	app.use(passport.session());
-	//Endpoints
+
+	//HTTP GET Endpoints
 	app.get('/', function(req, res){
 		if(req.user){
-		res.render(__dirname + '/pages/jade/index.jade', {user: req.user['name']});
+		res.render(__dirname + '/pages/jade/index.jade', {user: req.user['username']});
 		}
 		else{
 			res.render(__dirname + '/pages/jade/index.jade', {});
@@ -84,7 +85,7 @@ var auth = require('passport-local-authenticate');
 	});
 	app.get('/register', function(req, res){
 		if(req.user){
-		res.render(__dirname + '/pages/jade/register.jade', {user: req.user['name']});
+		res.render(__dirname + '/pages/jade/register.jade', {user: req.user['username']});
 		}
 		else{
 			res.render(__dirname + '/pages/jade/register.jade', {});
@@ -92,18 +93,15 @@ var auth = require('passport-local-authenticate');
 	});
 	app.get('/report', function(req, res){
 		if(req.user){
-		res.render(__dirname + '/pages/jade/report.jade', {user: req.user['name']});
+		res.render(__dirname + '/pages/jade/report.jade', {user: req.user['username']});
 		}
 		else{
 			res.render(__dirname + '/pages/jade/report.jade', {});
 		}
 	});
-	app.post('/login', passport.authenticate('local', {failureRedirect: '/register'}), function(req, res){
-		res.redirect('/');
-	});
 	app.get('/login', function(req, res){
 		if(req.user){
-		res.render(__dirname + '/pages/jade/login.jade', {user: req.user['name']});
+		res.render(__dirname + '/pages/jade/login.jade', {user: req.user['username']});
 		}
 		else{
 			res.render(__dirname + '/pages/jade/login.jade', {});
@@ -113,90 +111,109 @@ var auth = require('passport-local-authenticate');
 		req.logout();
 		res.redirect('back');
 	});
+	app.get('/user/:username', function(req, res){
+		var uname = req.params.username;
+		players.find({username: uname}, {password:0, _id:0}, function(err, docs){
+			if(err || !docs[0]){res.render(__dirname + '/pages/jade/unf.jade', {user: uname});}
+			else{res.render(__dirname + '/pages/jade/profile.jade', {userJson: JSON.stringify(docs[0])});}
+		});
+	});
+	//HTTP POST Endpoints
+	app.post('/login', passport.authenticate('local', {failureRedirect: '/register'}), function(req, res){
+		res.redirect('/');
+	});
+	//Client-server communication post page load
 	io.sockets.on('connection', function(socket){
+		//Spawn the initial scoreboard
 		buildScoreboard(function(data){
 			socket.emit('refreshScoreboard', {reason: 'firstConnect', scoreboardJson: data});
 		});
+		//When registering on the register page do this
 		socket.on('register', function(data){
+			//Booleans for if the user is joining that game or not
 			var melee = data['melee'];
 			var smashFour = data['smashFour'];
 			var pm = data['pm'];
+			//The new user's name and password
 			var newName = data['name'];
 			var pass = data['password'];
-			players.find({name: newName}, function(err, docs){
+			players.find({username: newName}, function(err, docs){
 				if(err){
+					//If there is an error finding the user, log it
 					socket.emit('register', {status: 0, reason: 'error'});
-					fs.writeFileSync(eloLogfile, '[' + moment().format('YYYY-MM-DD HH:mm:ss Z') + '] ' +'Error searching for name ' + newName + ' while registering\n');
+					fs.writeFileSync(eloLogfile, '[' + moment().format('YYYY-MM-DD HH:mm:ss Z') + '] ' +'Error searching for username ' + newName + ' while registering\n');
 				}
 				else if(docs[0]){
+					//If the user already exists tell the page to display an alert
 					socket.emit('register', {status: 0, reason: 'exists'});
 				}
 				else{
-					fs.writeFileSync(eloLogfile, '[' + moment().format('YYYY-MM-DD HH:mm:ss Z') + '] ' +'User ' + newName + ' created with rating of 1200\n');
 					auth.hash(pass, function(err, hashed){
-						if(err){socket.emit('register',{status:0, reason: 'error hashing'}); return;}
-						players.insert({name: newName, password: hashed}, function(){
-						if(melee){players.update({name:newName}, {$set: {meleeRating: 1200, meleeWins: 0, meleeLosses: 0, meleeMatchups: {}, meleeMain: data['meleeMain']}});}
-						if(smashFour){players.update({name:newName}, {$set: {smashFourRating: 1200, smashFourWins: 0, smashFourLosses: 0, smashFourMatchups: {}, smashFourMain: data['smashFourMain']}});}
-						if(pm){players.update({name:newName}, {$set: {pmRating: 1200, pmWins: 0, pmLosses: 0, pmMatchups: {}, pmMain: data['pmMain']}});}
+						if(err){socket.emit('register',{status:0, reason: 'error hashing'}); return;} //if there is an error hashing the password tell the page to alert
+						players.insert({username: newName, displayName: newName, password: hashed}, function(){
+						fs.writeFileSync(eloLogfile, '[' + moment().format('YYYY-MM-DD HH:mm:ss Z') + '] ' +'User ' + newName + ' created\n'); //Log the creation of a new user
+						//Add melee, smash 4 and/or pm to the user
+						if(melee){players.update({username:newName}, {$set: {meleeRating: 1200, meleeWins: 0, meleeLosses: 0, meleeMatchups: {}, meleeMain: data['meleeMain']}});}
+						if(smashFour){players.update({username:newName}, {$set: {smashFourRating: 1200, smashFourWins: 0, smashFourLosses: 0, smashFourMatchups: {}, smashFourMain: data['smashFourMain']}});}
+						if(pm){players.update({username:newName}, {$set: {pmRating: 1200, pmWins: 0, pmLosses: 0, pmMatchups: {}, pmMain: data['pmMain']}});}
 						});
+						//Tell the client it succeded
 						socket.emit('register', {status: 1, reason: 'success'});
 						buildScoreboard(function(data){
+							//Refresh all scoreboards
 							io.emit('refreshScoreboard', {reason: 'newUser', scoreboardJson: data});
 						});
 					});
 				}
 			});
 		});
+		//When reporting a match on the report page do this
 		socket.on('report', function(data){
+			//The two players in the game and the game played
 			var playerOne = data['playerOne'];
 			var playerTwo = data['playerTwo'];
-			var playerOneStuID = data['playerOneId'];
-			var playerTwoStuID = data['playerTwoId'];
 			var gamePlayed = data['game'];
-			players.find({name: playerOne}, function(err, playerOneEntry){
-				if(err){
+			players.find({username: playerOne}, function(err1, playerOneEntry){
+				if(err1){
+					//If there is an error finding the player, log it and tell the client
 					fs.writeFileSync(eloLogfile, '[' + moment().format('YYYY-MM-DD HH:mm:ss Z') + '] ' +'Error finding player ' + playerOne + '\n');
 					socket.emit('report', {status: 0, reason: 'error'});
 				}
-				else if(!playerOneEntry[0]){
-					socket.emit('report', {status: 0, reason: 'playerDoesNotExist', playerName: playerOne});
-				}
 				else if(!playerOneEntry[0][gamePlayed + 'Rating']){
+					//If the player is not registered for this game tell the client
 					socket.emit('report', {status: 0, reason: 'playerNotRegistered', playerName: playerOne});
 				}
-				else if(playerOneEntry[0]['studentID'] != playerOneStuID){
-					socket.emit('report', {status: 0, reason: 'wrongPassword', playerName: playerOne});
-				}
 				else{
-					err = 0;
-					players.find({name: playerTwo}, function(err, playerTwoEntry){
-						if(err){
+					players.find({username: playerTwo}, function(err2, playerTwoEntry){
+						if(err2){
+							//If there is an error finding the player, log it and tell the client
 							fs.writeFileSync(eloLogfile, '[' + moment().format('YYYY-MM-DD HH:mm:ss Z') + '] ' +'Error finding player ' + playerTwo + '\n');
 							socket.emit('report', {status: 0, reason: 'error'});
 						}
 						else if(!playerTwoEntry[0]){
+							//If player two does not exist send the client an alert
 							socket.emit('report', {status: 0, reason: 'playerDoesNotExist', playerName: playerTwo});
 						}
 						else if(!playerTwoEntry[0][gamePlayed + 'Rating']){
+							//If the player is not registered for this game tell the client
 							socket.emit('report', {status: 0, reason: 'playerNotRegistered', playerName: playerTwo});
 						}
-						else if(playerTwoEntry[0]['studentID'] != playerTwoStuID){
-							socket.emit('report', {status: 0, reason: 'wrongPassword', playerName: playerTwo});
-						}
 						else{
+							//Pull the game data from the initial form
 							var playerOneGamesWon = parseInt(data['playerOneGamesWon']);
 							var playerTwoGamesWon = parseInt(data['playerTwoGamesWon']);
 							var numGamesPlayed = playerOneGamesWon + playerTwoGamesWon;
+							//Pull some data from the two returned documents from the database queries
 							var playerOneRatingBefore = playerOneEntry[0][gamePlayed + 'Rating'];
-							var playerOneID = playerOneEntry[0]['_id'].toString().replace('ObjectID("', '').slice(0, -1);
 							var playerTwoRatingBefore = playerTwoEntry[0][gamePlayed + 'Rating'];
-							var playerTwoID = playerTwoEntry[0]['_id'].toString().replace('ObjectID("', '').slice(0, -1);
+							//Calculate the expected outcome
 							var playerOneScore = playerOneGamesWon / numGamesPlayed;
 							var playerTwoScore = playerTwoGamesWon / numGamesPlayed;
 							var playerOneExpected = 1 / (1 + Math.pow(10, ((playerTwoRatingBefore - playerOneRatingBefore)/400)));
 							var playerTwoExpected = 1 / (1 + Math.pow(10, ((playerOneRatingBefore - playerTwoRatingBefore)/400)));
+							//Calculate the K Value (How much each player can win or lose from this game or set)
 							var kVal = (Math.log(numGamesPlayed + 1) / Math.log(1.1))*4;
+							//Add the kValue times the difference in performance to the player's rating (rating = rating + kVal*(actualScore - expectedScore)). If it is within the user's first 10 games then make it count twice as much.
 							if(playerOneEntry[0][gamePlayed + 'Wins'] + playerOneEntry[0][gamePlayed + 'Losses'] <= 10){
 								var playerOneNewRating = playerOneRatingBefore + Math.floor(2*kVal*(playerOneScore - playerOneExpected));
 							}
@@ -209,30 +226,35 @@ var auth = require('passport-local-authenticate');
 							else{
 								var playerTwoNewRating = playerTwoRatingBefore + Math.floor(kVal*(playerTwoScore - playerTwoExpected));
 							}
-							matches.insert({IDs: [playerOneID, playerTwoID], ratingsBefore: [playerOneRatingBefore, playerTwoRatingBefore], ratingsAfter: [playerOneNewRating, playerTwoNewRating], game: gamePlayed});
+							//Insert the recording of the match into the matches collection
+							matches.insert({usernames: [playerOne, playerTwo], ratingsBefore: [playerOneRatingBefore, playerTwoRatingBefore], ratingsAfter: [playerOneNewRating, playerTwoNewRating], game: gamePlayed});
+							//Build the JSON for the update to the players collection
 							var playerOneJson = {$set:{}};
-							if(!playerOneEntry[0][gamePlayed + 'Matchups'][playerTwoID]){
-								playerOneEntry[0][gamePlayed + 'Matchups'][playerTwoID] = {wins:0,losses:0};
+							if(!playerOneEntry[0][gamePlayed + 'Matchups'][playerTwo]){
+								playerOneEntry[0][gamePlayed + 'Matchups'][playerTwo] = {wins:0,losses:0};
 							}
-							playerOneJson['$set'][gamePlayed + 'Matchups.' + playerTwoID + ".wins"] = playerOneEntry[0][gamePlayed + 'Matchups'][playerTwoID]['wins'] + playerOneGamesWon;
-							playerOneJson['$set'][gamePlayed + 'Matchups.' + playerTwoID + ".losses"] = playerOneEntry[0][gamePlayed + 'Matchups'][playerTwoID]['losses'] + playerTwoGamesWon;
+							playerOneJson['$set'][gamePlayed + 'Matchups.' + playerTwo + ".wins"] = playerOneEntry[0][gamePlayed + 'Matchups'][playerTwo]['wins'] + playerOneGamesWon;
+							playerOneJson['$set'][gamePlayed + 'Matchups.' + playerTwo + ".losses"] = playerOneEntry[0][gamePlayed + 'Matchups'][playerTwo]['losses'] + playerTwoGamesWon;
 							playerOneJson['$set'][gamePlayed + 'Rating'] =  playerOneNewRating;
 							playerOneJson['$set'][gamePlayed + 'Wins'] = playerOneEntry[0][gamePlayed + 'Wins'] + playerOneGamesWon;
 							playerOneJson['$set'][gamePlayed + 'Losses'] = playerOneEntry[0][gamePlayed + 'Losses'] + playerTwoGamesWon;
 							var playerTwoJson = {$set:{}};
-							if(!playerTwoEntry[0][gamePlayed + 'Matchups'][playerOneID]){
-								playerTwoEntry[0][gamePlayed + 'Matchups'][playerOneID] = {wins:0,losses:0};
+							if(!playerTwoEntry[0][gamePlayed + 'Matchups'][playerOne]){
+								playerTwoEntry[0][gamePlayed + 'Matchups'][playerOne] = {wins:0,losses:0};
 							}
-							playerTwoJson['$set'][gamePlayed + 'Matchups.' + playerOneID + ".wins"] = playerTwoEntry[0][gamePlayed + 'Matchups'][playerOneID]['wins'] + playerTwoGamesWon;
-							playerTwoJson['$set'][gamePlayed + 'Matchups.' + playerOneID + ".losses"] = playerTwoEntry[0][gamePlayed + 'Matchups'][playerOneID]['losses'] + playerOneGamesWon;
+							playerTwoJson['$set'][gamePlayed + 'Matchups.' + playerOne + ".wins"] = playerTwoEntry[0][gamePlayed + 'Matchups'][playerOne]['wins'] + playerTwoGamesWon;
+							playerTwoJson['$set'][gamePlayed + 'Matchups.' + playerOne + ".losses"] = playerTwoEntry[0][gamePlayed + 'Matchups'][playerOne]['losses'] + playerOneGamesWon;
 							playerTwoJson['$set'][gamePlayed + 'Rating'] = playerTwoNewRating;
 							playerTwoJson['$set'][gamePlayed + 'Wins'] = playerTwoEntry[0][gamePlayed + 'Wins'] + playerTwoGamesWon;
 							playerTwoJson['$set'][gamePlayed + 'Losses'] = playerTwoEntry[0][gamePlayed + 'Losses'] + playerOneGamesWon;
-							players.update({name: playerOne}, playerOneJson);
-							players.update({name: playerTwo}, playerTwoJson);
+							fs.writeFileSync(eloLogfile, '[' + moment().format('YYYY-MM-DD HH:mm:ss Z') + '] ' + 'Match reported between ' + playerOne + ' and ' + playerTwo + '\n');
+							//Send the update queries to the DB
+							players.update({username: playerOne}, playerOneJson);
+							players.update({username: playerTwo}, playerTwoJson);
+							//Report success to the client
 							socket.emit('report', {status: 1, reason: 'success'});
 							buildScoreboard(function(data){
-								io.emit('refreshScoreboard', {reason: 'newUser', scoreboardJson: data});
+								io.emit('refreshScoreboard', {reason: 'matchReported', scoreboardJson: data});
 							});
 						}
 					});
